0f188a86b688d403d36cddb8c2dfdf21b335b751
Francis James Tolentino <francistolentino1107@gmail.com>
2022-10-25 18:45:50 +0800

    asdfasdf

--- dist/index.js
+++ dist/index.js
@@ -7,12 +7,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
+const ws_1 = __importDefault(require("ws"));
 dotenv_1.default.config();
-const app = (0, express_1.default)();
 const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 4000;
-app.get("/", (0, cors_1.default)(), (_req, res) => {
-    res.send("hello world");
+const app = (0, express_1.default)();
+const wsServer = new ws_1.default.Server({ noServer: true });
+app.use((0, cors_1.default)());
+let connectedUsers = 0;
+const usernames = [];
+const connect = (json, socket) => {
+    if (usernames.includes(json.username))
+        return;
+    usernames.push(json.username);
+    connectedUsers++;
+    socket.send(JSON.stringify({
+        id: connectedUsers,
+    }));
+};
+const checkIfConnected = (json, _socket) => {
+    if (usernames.includes(json.username))
+        return;
+};
+wsServer.on("connection", (socket) => {
+    if (connectedUsers === 2) {
+        socket.close();
+        return;
+    }
+    socket.on("message", (payload) => {
+        const json = JSON.parse(payload.toString());
+        if ('username' in json)
+            checkIfConnected(json, socket);
+        if ('connect' in json)
+            connect(json, socket);
+    });
 });
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
+});
+server.on("upgrade", (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, (socket) => {
+        wsServer.emit("connection", socket, request);
+    });
 });
--- index.ts
+++ index.ts
@@ -1,17 +1,54 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import ws from "ws";
 
 dotenv.config();
 
-const app: Express = express();
 const PORT = process.env.PORT ?? 4000;
+const app: Express = express();
+const wsServer = new ws.Server({ noServer: true });
 
-app.get("/", cors(), (_req: Request, res: Response) => {
-  res.send("hello world");
+app.use(cors());
+
+let connectedUsers = 0;
+const usernames: string[] = [];
+
+const connect = (json: any, socket: any) => {
+  if (usernames.includes(json.username)) return;
+  usernames.push(json.username);
+  connectedUsers++;
+  socket.send(JSON.stringify({
+    id: connectedUsers,
+  }));
+}
+
+const checkIfConnected = (json: any, _socket: any) => {
+  if (usernames.includes(json.username)) return;
+}
+
+wsServer.on("connection", (socket) => {
+  
+  if (connectedUsers === 2) {
+    socket.close();
+    return;
+  }
+
+  socket.on("message", (payload) => {
+    const json = JSON.parse(payload.toString());
+    if ('username' in json) checkIfConnected(json, socket);
+    if ('connect' in json) connect(json, socket);
+  });
 });
 
 
-app.listen(PORT, () => {
+
+const server = app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
+});
+
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
+  });
 });
